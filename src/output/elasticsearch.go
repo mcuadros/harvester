c97417e8e856c7838aefeb1d75c83292ec7c2359
Carlos Cobo <toqueteos@gmail.com>
2016-01-13 18:35:20 +0100

    _id can't be changed on Elastic v2, use _uid instead

--- src/output/elasticsearch.go
+++ src/output/elasticsearch.go
@@ -7,27 +7,25 @@ import (
 	"github.com/mcuadros/harvester/src/intf"
 )
 
-const ESIdField = "_id"
-
 type ElasticsearchConfig struct {
-	Host    string `default:"localhost" description:"elastic host port "`
-	Port    int    `default:"9200" description:"elastic search port "`
-	Index   string `description:"index name"`
-	Type    string `description:"index type"`
-	Timeout int    `default:"1" description:"contection timeout"`
-	IdField string `description:"the content of the given field will be copied into _id"`
+	Host     string `default:"localhost" description:"elastic host port "`
+	Port     int    `default:"9200" description:"elastic search port "`
+	Index    string `description:"index name"`
+	Type     string `description:"index type"`
+	Timeout  int    `default:"1" description:"contection timeout"`
+	UidField string `description:"copied as id into into _uid, which consists of 'type#id'"`
 }
 
 type Elasticsearch struct {
 	HTTP
-	idField string
+	uidField string
 }
 
 func NewElasticsearch(config *ElasticsearchConfig) *Elasticsearch {
 	defaults.SetDefaults(config)
 
 	output := new(Elasticsearch)
-	output.idField = config.IdField
+	output.uidField = config.UidField
 	output.SetConfig(output.TransformConfig(config))
 
 	return output
@@ -46,10 +44,6 @@ func (o *Elasticsearch) TransformConfig(config *ElasticsearchConfig) *HTTPConfig
 }
 
 func (o *Elasticsearch) PutRecord(record intf.Record) bool {
-	if o.idField != "" {
-		record[ESIdField] = record[o.idField]
-	}
-
 	return o.HTTP.PutRecord(record)
 }
 
@@ -61,8 +55,8 @@ func (o *Elasticsearch) getIndexURL(config *ElasticsearchConfig) string {
 		config.Type,
 	)
 
-	if o.idField != "" {
-		url = fmt.Sprintf("%s/%%{%s}", url, o.idField)
+	if o.uidField != "" {
+		url = fmt.Sprintf("%s/%%{%s}", url, o.uidField)
 	}
 
 	return url
